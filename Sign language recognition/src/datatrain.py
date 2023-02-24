b50275947b9d308d1fe7729a8665e32e278f1f30
M.J.M Sakeen <jaleel.20211199@iit.ac.lk>
2023-02-24 17:02:36 +0530

    Created test code data prediction

--- Sign language recognition/src/datatrain.py
+++ Sign language recognition/src/datatrain.py
@@ -1,5 +1,13 @@
 import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+tf.config.set_visible_devices(physical_devices[0], 'GPU')
+
 from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.applications import MobileNet
 
 # Define data directories
 train_dir = 'Data/BSL'
@@ -7,10 +15,18 @@ val_dir = 'Data/BSL'
 
 # Define image dimensions and batch size
 img_width, img_height = 224, 224
-batch_size = 32
+batch_size = 16
 
 # Define ImageDataGenerator and rescale the pixel values to be in the range [0,1]
-train_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=20,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+
 val_datagen = ImageDataGenerator(rescale=1./255)
 
 # Load training data and apply data augmentation
@@ -30,27 +46,32 @@ val_data = val_datagen.flow_from_directory(
 # Define the number of classes in your dataset
 num_classes = train_data.num_classes
 
-# Load pre-trained VGG16 model without the top (fully connected) layers
-base_model = tf.keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3))
+# Load pre-trained MobileNet model without the top (fully connected) layers
+base_model = MobileNet(weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3))
 
 # Add your own classification layer
 x = base_model.output
-x = tf.keras.layers.GlobalAveragePooling2D()(x)
-x = tf.keras.layers.Dense(1024, activation='relu')(x)
-predictions = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(num_classes, activation='softmax')(x)
 
 # Define the new model
-model = tf.keras.models.Model(inputs=base_model.input, outputs=predictions)
+model = Model(inputs=base_model.input, outputs=predictions)
 
 # Freeze the pre-trained layers (optional)
 for layer in base_model.layers:
     layer.trainable = False
 
 # Compile the model with a low learning rate
-model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Define a learning rate scheduler
+lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-4 * 10**(epoch/20))
 
 # Train the model on your dataset
-model.fit(train_data, epochs=10, validation_data=val_data)
+model.fit(train_data, epochs=10, validation_data=val_data, callbacks=[lr_scheduler])
 
-# Save the model to a .h5 file
-model.save('keras.h5')
+# Save the model
+model.save('BSL_model.h5')
+
+
