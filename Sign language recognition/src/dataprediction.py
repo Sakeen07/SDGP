7e4d2295f72c2f0a948bcc679a4499407ec379f7
Sakeen07 <108605060+Sakeen07@users.noreply.github.com>
2023-03-26 18:47:08 +0530

    Update dataprediction.py
    Complete the dataprediction file

--- Sign language recognition/src/dataprediction.py
+++ Sign language recognition/src/dataprediction.py
@@ -1,197 +1,58 @@
 import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
 import numpy as np
-import os
+import math
 
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+offset = 20
+imgSize = 600
+classifier = Classifier("Model.h5", "Model.txt")
 
-def nothing(x):
-    pass
+labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+          "W", "X", "Y", "Z"]
 
-
-image_x, image_y = 64, 64
-
-from keras.models import load_model
-
-classifier = load_model('Model.h5')
-
-# original = cv2.imread("5.png")
-fileEntry = []
-for file in os.listdir("Data/Final Data"):
-    if file.endswith(".png"):
-        fileEntry.append(file)
-
-
-def imgprocessing():
-    image_to_compare = cv2.imread("./Data/Final Data/A/0.jpg")
-    original = cv2.imread("1.png")
-    sift = cv2.xfeatures2d.SIFT_create()
-    kp_1, desc_1 = sift.detectAndCompute(original, None)
-    kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
-
-    index_params = dict(algorithm=0, trees=5)
-    search_params = dict()
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-    matches = flann.knnMatch(desc_1, desc_2, k=2)
-
-    good_points = []
-    ratio = 0.6
-    for m, n in matches:
-        if m.distance < ratio * n.distance:
-            good_points.append(m)
-    # print(len(good_points))
-    fin = len(kp_1) - len(kp_2)
-    print(abs(fin))
-    if abs(fin) <= 2:
-        return 'space'
-
-    result = cv2.drawMatches(original, kp_1, image_to_compare, kp_2, good_points, None)
-
-    # cv2.imshow("result", result)
-    # cv2.imshow("Original", original)
-    # cv2.imshow("Duplicate", image_to_compare)
-    # cv2.destroyAllWindows()
-
-
-def predictor():
-    import numpy as np
-    from keras.preprocessing import image
-    test_image = image.load_img('1.png', target_size=(64, 64))
-    test_image = image.img_to_array(test_image)
-    test_image = np.expand_dims(test_image, axis=0)
-    result = classifier.predict(test_image)
-    for i in range(len(fileEntry)):
-        image_to_compare = cv2.imread("./SampleGestures/" + fileEntry[i])
-        original = cv2.imread("1.png")
-        sift = cv2.xfeatures2d.SIFT_create()
-        kp_1, desc_1 = sift.detectAndCompute(original, None)
-        kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
-
-        index_params = dict(algorithm=0, trees=5)
-        search_params = dict()
-        flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-        matches = flann.knnMatch(desc_1, desc_2, k=2)
-
-        good_points = []
-        ratio = 0.6
-        for m, n in matches:
-            if m.distance < ratio * n.distance:
-                good_points.append(m)
-        print(fileEntry[i])
-        if (abs(len(good_points) + len(matches)) > 20):
-            gesname = fileEntry[i]
-            gesname = gesname.replace('.png', '')
-            return gesname
-    # print(good_points)
-    # if(abs(fin)<=2):
-    # return 'space'
-    if result[0][0] == 1:
-        return 'A'
-    elif result[0][1] == 1:
-        return 'B'
-    elif result[0][2] == 1:
-        return 'C'
-    elif result[0][3] == 1:
-        return 'D'
-    elif result[0][4] == 1:
-        return 'E'
-    elif result[0][5] == 1:
-        return 'F'
-    elif result[0][6] == 1:
-        return 'G'
-    elif result[0][7] == 1:
-        return 'H'
-    elif result[0][8] == 1:
-        return 'I'
-    elif result[0][9] == 1:
-        return 'J'
-    elif result[0][10] == 1:
-        return 'K'
-    elif result[0][11] == 1:
-        return 'L'
-    elif result[0][12] == 1:
-        return 'M'
-    elif result[0][13] == 1:
-        return 'N'
-    elif result[0][14] == 1:
-        return 'O'
-    elif result[0][15] == 1:
-        return 'P'
-    elif result[0][16] == 1:
-        return 'Q'
-    elif result[0][17] == 1:
-        return 'R'
-    elif result[0][18] == 1:
-        return 'S'
-    elif result[0][19] == 1:
-        return 'T'
-    elif result[0][20] == 1:
-        return 'U'
-    elif result[0][21] == 1:
-        return 'V'
-    elif result[0][22] == 1:
-        return 'W'
-    elif result[0][23] == 1:
-        return 'X'
-    elif result[0][24] == 1:
-        return 'Y'
-    elif result[0][25] == 1:
-        return 'Z'
-
-
-cam = cv2.VideoCapture(0)
-
-cv2.namedWindow("Trackbars")
-
-cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
-cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
-cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
-cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
-
-cv2.namedWindow("Sign language Recognition")
-
-img_text = ''
-img_text1 = ''
-# z5=1
 while True:
-    ret, frame = cam.read()
-    frame = cv2.flip(frame, 1)
-    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
-    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
-    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
-    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
-    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
-    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+    success, img = cap.read()
+    img_output = img.copy()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
 
-    img = cv2.rectangle(frame, (425, 100), (625, 300), (0, 255, 0), thickness=2, lineType=8, shift=0)
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+        imgCropShape = imgCrop.shape
+        aspectRatio = h / w
 
-    lower_blue = np.array([l_h, l_s, l_v])
-    upper_blue = np.array([u_h, u_s, u_v])
-    imcrop = img[102:298, 427:623]
-    hsv = cv2.cvtColor(imcrop, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            print(prediction, index)
 
-    cv2.putText(frame, img_text, (30, 400), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (0, 255, 0))
-    cv2.putText(frame, img_text1, (80, 400), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (0, 255, 0))
-    cv2.imshow("ASL Recognition", frame)
-    cv2.imshow("mask", mask)
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+        cv2.putText(img_output, labels[index], (x, y - 20), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 2)
+        cv2.rectangle(img_output, (x - offset, y - offset), (x + w + offset, y + h + offset), (255, 0, 255), 4)
 
-    img_name = "1.png"
-    save_img = cv2.resize(mask, (image_x, image_y))
-    cv2.imwrite(img_name, save_img)
-    img_text = predictor()
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
 
-    if cv2.waitKey(1) == 27:
-        break
+    cv2.imshow("Image", img_output)
+    cv2.waitKey(1)
 
-# img_text1 = imgprocessing()
-
-
-cam.release()
-cv2.destroyAllWindows()
-# Combine the letter with the different prediction to create a word
 # Connect the text word to the AI generated voice
 # Connect the voice and text with the translation
 
