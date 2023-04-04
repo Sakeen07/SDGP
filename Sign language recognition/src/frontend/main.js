b93b7b2c9ebbf4c884236ac6d94c7a89bb9b030f
Ray <ahamedrayyan06@gmail.com>
2023-04-04 21:56:51 +0530

    Connecting mediapipe to JS

--- Sign language recognition/src/frontend/main.js
+++ Sign language recognition/src/frontend/main.js
@@ -214,4 +214,27 @@ navigator.mediaDevices
            }
            canvasCtx.restore();
          }
-        
+
+         const hands = new Hands({locateFile: (file)
+         => {
+         return 'https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}';
+
+         }});
+         hands.setOptions({
+            maxNumHands:2,
+            modelComplexity:1,
+            minDetectionConfidence:0.5,
+            minTrackingConfidence:0.5
+            });
+
+            hands.onResults(onResults);
+
+            const camera = new Camera(videoElement,{
+            onFrame: async() => {
+            await hands.send({image: videoElement});
+            },
+            width:1280,
+            height:720
+            });
+            camera.start();
+
