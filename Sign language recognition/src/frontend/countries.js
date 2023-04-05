a8f0b0910ed7b917a7ddc496148374f622cdf60e
Damma-Github <60842023+Damma-git@users.noreply.github.com>
2023-04-05 21:06:59 +0530

    Adding css stylings and add translate tab

---
+++ Sign language recognition/src/frontend/countries.js
@@ -0,0 +1,14 @@
+const countries ={
+    "am-ET": "Amharic",
+    "ar-SA":"Arabic",
+    "be-BY": "Bielarus",
+    "en-GB":"English",
+    "es-ES":"Spanish",
+    "de-DE":"German",
+    "si-SI":"Sinhalese"
+
+
+    
+    
+
+}
