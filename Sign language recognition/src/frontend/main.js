7491ee1bbd2c3a7acaa5e17c13e31d9ea77bfc24
LAPTOP-D7UU0MU8\acer <pussepitiyavinuka@gmail.com>
2023-04-05 20:50:13 +0530

    Initial styling

--- Sign language recognition/src/frontend/main.js
+++ Sign language recognition/src/frontend/main.js
@@ -1,6 +1,11 @@
 // Get a reference to the video element and the label element
         const label = document.getElementById('label');
         const video = document.getElementById('videoStream');
+        const button = document.getElementById('btn');
+
+        let sentense = [];
+        let letters = [];
+        let letter = "";
 
         // Set the interval time (in milliseconds)
         const intervalTime = 1000;
@@ -9,25 +14,35 @@
         const endpointUrl = 'http://127.0.0.1:8080/label';
         const videoUrl = 'http://127.0.0.1:8080/video_feed';
 
-        // function getVideoFromEndpoint() {
-        //   fetch(videoUrl)
-        //   .then(response => response.json())
-        //   .then(data => {
-        //     video.src
-        //   })
-        // }
-
         function getValueFromEndpoint() {
           fetch(endpointUrl)
             .then(response => response.json())
             .then(data => {
               // Do something with the retrieved value
               label.innerText = `Label: ${data}`;
+              // console.log(data)
               console.log('Retrieved value:', data);
+              letter = data
+              // button.addEventListener('click', function(){
+              //   sentense.push(data)
+              //   console.log(sentense);
+              // })
             })
+            // , creatSentense(letter)
             .catch(error => {
               // Handle any errors that occur during the request
               console.error('Error retrieving value:', error);
+            });
+            creatSentense(letter)
+          }
+
+          
+
+          function creatSentense(label) {
+            console.log(letter);
+            button.addEventListener('click', function() {
+              sentense.push(label)
+              console.log(sentense);
             });
           }
 
